243c76fa7823b6c8a81e4de1b15d2aab12eedec2
war <wiltonconradchaimberlainthird@gmail.com>
2025-05-05 02:46:42 -0700

    📌 Auto-commit snapshot (temp-live sync)

--- frontend/src/pages/CommandPanel.tsx
+++ frontend/src/pages/CommandPanel.tsx
@@ -1,23 +1,18 @@
-import ChatPanel from "@/components/planning/ChatPanel";
-import PhaseSidebar from "@/components/planning/PhaseSidebar";
-import TabbedInfoPanel from "@/components/planning/TabbedInfoPanel";
+import React from 'react';
+import Sidebar from '../components/commandPanel/Sidebar';
+import ChatPanel from '../components/commandPanel/ChatPanel';
+import TabbedPanel from '../components/commandPanel/TabbedPanel';
 
-export default function CommandPanel() {
+const CommandPanel: React.FC = () => {
   return (
-    <div className="flex h-screen w-full">
-      <aside className="w-20 border-r bg-gray-50 p-2">
-        <PhaseSidebar />
-      </aside>
-
-      <main className="flex flex-col flex-1">
-        <section className="flex-1 overflow-y-auto p-4 border-b">
-          <ChatPanel />
-        </section>
-
-        <section className="h-[40%] border-t bg-white p-2">
-          <TabbedInfoPanel />
-        </section>
-      </main>
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex flex-col flex-1">
+        <ChatPanel />
+        <TabbedPanel />
+      </div>
     </div>
   );
-}
+};
+
+export default CommandPanel;
